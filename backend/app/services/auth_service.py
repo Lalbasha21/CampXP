8c934084688d006583a3f170a304f1bc6f5cce5f
Lal Basha <klalbasha2@gmail.com>
2026-05-23 10:54:53 +0530

    Implemented student login service with JWT authentication

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import Session
 from app.models.student_model import Student
 from app.schemas.auth_schema import StudentRegister
 from app.utils.helpers import hash_password
-
+from app.schemas.auth_schema import LoginRequest
+from app.utils.helpers import verify_password
+from app.utils.jwt_handler import create_access_token
 
 def register_student(db: Session, student: StudentRegister):
 
@@ -25,3 +27,38 @@ def register_student(db: Session, student: StudentRegister):
     db.refresh(new_student)
 
     return new_student
+
+
+def login_student(db: Session, login_data: LoginRequest):
+    student = db.query(Student).filter(
+        Student.email == login_data.email
+    ).first()
+
+    if not student:
+        return {
+            "error" : "Invalid email"
+        }
+    valid_pasword = verify_password(
+        login_data.password,
+        student.password
+    )
+    
+
+    if not valid_password:
+        return {
+            "error" : "Invalid password"
+        }
+    token = create_access_token({
+        "student_id": student.id,
+        "email": student.email
+    })
+
+    return {
+        "message": "Login successful",
+        "access_token": token,
+        "student": {
+            "id": student.id,
+            "name": student.full_name,
+            "email": student.email
+        }
+    }
